27ec238e95d048efaa77366570b86fd9193b42be
lexuantien <tienlx97@gmail.com>
2022-05-06 09:44:15 +0700

    feat(@hooks): add usePersistedState
    Signed-off-by: lexuantien <tienlx97@gmail.com>

--- packages/shared/hooks/index.ts
+++ packages/shared/hooks/index.ts
@@ -1,2 +1,3 @@
 export { default as useScrollDisabler } from './src/use-scroll-disabler.hook';
 export { default as useDebouncedValues } from './src/use-debounced-values.hook';
+export { default as usePersistedState } from './src/use-persisted-state.hook';
