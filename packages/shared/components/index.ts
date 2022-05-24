0bc07e031bcea111772cf11bbaa880cc68eec177
lexuantien <tienlx97@gmail.com>
2022-05-24 10:34:08 +0700

    chore(component): change import file
    Signed-off-by: lexuantien <tienlx97@gmail.com>

--- packages/shared/components/index.ts
+++ packages/shared/components/index.ts
@@ -1,44 +1,9 @@
-// export { UnstyledButton, VisuallyHidden } from './src/button';
-// export { default as SettingCheckbox } from './src/setting-checkbox/SettingCheckbox';
-
-// export { InPortal } from './src/inportal';
-// export { BaseWrapper } from './src/sidenote';
-// export { Spinner } from './src/spinner';
-// export { Tooltip } from './src/tooltip';
-// export { ProgressBar } from './src/progressbar';
-// export { Playground } from './src/playground';
-// export {
-//   ConfigContext,
-//   ConfigProvider,
-//   loadDefaultThemeBeforeReactRender,
-// } from './src/config-context';
-// export { useCssReset, useGlobalCss } from './src/global-styles';
-// export * from './src/icons';
-// export { Paragraph } from './src/paragraph';
-// export { InlineCode } from './src/inlinecode';
-// export { Em } from './src/em';
-// export { Blockquote } from './src/blockquote';
-// export { List } from './src/list';
-// export { PostImage } from './src/postimage';
-// export { PostLink } from './src/link';
-// export { ContentHeading } from './src/contentheading';
-// export { CodeSnippet } from './src/codesnippet';
-// export { SideNote } from './src/sidenote';
-// export { VideoGif } from './src/videogif';
-// export { YoutubeEmbed } from './src/youtube-embed';
-
-// export { ContentContext, ContentProvider } from './src/content-context';
-
 // =========================== //
-
-import { UnstyledButton, VisuallyHidden } from './src/button';
-import { default as SettingCheckbox } from './src/setting-checkbox/SettingCheckbox';
 
 import { InPortal } from './src/inportal';
 import { BaseWrapper, SideNote } from './src/sidenote';
 import { Spinner } from './src/spinner';
 import { Tooltip } from './src/tooltip';
-import { ProgressBar } from './src/progressbar';
 import { Playground } from './src/playground';
 import {
   ConfigContext,
@@ -58,67 +23,88 @@ import { CodeSnippet } from './src/codesnippet';
 import { VideoGif } from './src/videogif';
 import { YoutubeEmbed } from './src/youtube-embed';
 import { ContentContext, ContentProvider } from './src/content-context';
+import { ContentFooterArticle } from './src/content-footer-article';
+import { ContentFooter } from './src/content-footer-tutorial';
+import { Divider } from './src/divider';
+import { Heading } from './src/heading';
+import { HorizontalRule } from './src/horizontalrule';
+import { Strikethrough } from './src/strikethrough';
 
+//
 const R = {
+  // UnstyledButton,
+  // VisuallyHidden,
+
   ContentContext,
   ContentProvider,
-  UnstyledButton,
-  VisuallyHidden,
-  SettingCheckbox,
-  InPortal,
-  BaseWrapper,
-  Spinner,
-  Tooltip,
-  ProgressBar,
-  Playground,
+
   ConfigContext,
   ConfigProvider,
   loadDefaultThemeBeforeReactRender,
-  useCssReset,
-  useGlobalCss,
-  Paragraph,
-  InlineCode,
-  Em,
+
   Blockquote,
-  List,
-  PostImage,
-  PostLink,
-  ContentHeading,
   CodeSnippet,
+  ContentFooterArticle,
+  ContentFooter,
+  ContentHeading,
+  Divider,
+  Em,
+  Heading,
+  HorizontalRule,
+  InlineCode,
+  PostLink,
+  List,
+  Paragraph,
+  Playground,
+  PostImage,
   SideNote,
+  Strikethrough,
   VideoGif,
   YoutubeEmbed,
+  Spinner,
+  Tooltip,
+
+  BaseWrapper,
+  useCssReset,
+  useGlobalCss,
+
+  InPortal,
 };
 
 export default R;
 
 export {
+  // UnstyledButton,
+  // VisuallyHidden,
+
   ContentContext,
   ContentProvider,
-  UnstyledButton,
-  VisuallyHidden,
-  SettingCheckbox,
-  InPortal,
-  BaseWrapper,
-  Spinner,
-  Tooltip,
-  ProgressBar,
-  Playground,
   ConfigContext,
   ConfigProvider,
   loadDefaultThemeBeforeReactRender,
-  useCssReset,
-  useGlobalCss,
-  Paragraph,
-  InlineCode,
-  Em,
   Blockquote,
-  List,
-  PostImage,
-  PostLink,
-  ContentHeading,
   CodeSnippet,
+  ContentFooterArticle,
+  ContentFooter,
+  ContentHeading,
+  Divider,
+  Em,
+  Heading,
+  HorizontalRule,
+  InlineCode,
+  PostLink,
+  List,
+  Paragraph,
+  Playground,
+  PostImage,
   SideNote,
+  Strikethrough,
   VideoGif,
   YoutubeEmbed,
+  Spinner,
+  Tooltip,
+  BaseWrapper,
+  useCssReset,
+  useGlobalCss,
+  InPortal,
 };
