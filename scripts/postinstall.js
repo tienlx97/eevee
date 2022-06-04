ab806ee5818b8aed21d93eedf24b2205751b7126
lexuantien <tienlx97@gmail.com>
2022-06-04 19:46:47 +0700

    chore: test
    Signed-off-by: lexuantien <tienlx97@gmail.com>

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -8,7 +8,7 @@ main();
 
 function main() {
   // git v2.9.0 supports a custom hooks directory. This means we just need to check in the hooks scripts.
-  spawnSync('git', ['config', 'core.hooksPath', '.githooks']);
+  spawnSync('git', ['config', 'core.hooksPath', 'githooks']); 
 
   console.log('\n');
   console.log(COMMAND_PREFIX);
