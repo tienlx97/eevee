98d0e8e073282d149cd90b312cfe18ec70573943
Shyn <shyn.wallace@gmail.com>
2022-06-23 15:12:51 +0700

    fix: nelify redirect file
    Signed-off-by: Shyn <shyn.wallace@gmail.com>

--- packages/react-web/webpack/webpack.common.js
+++ packages/react-web/webpack/webpack.common.js
@@ -54,7 +54,7 @@ const commonConfig = {
     miniCssExtactPlugin,
     definePlugin,
     forkTsPlugin,
-    new CopyPlugin([{ from: 'src/pwa', to: '' }]),
+    new CopyPlugin([{ from: 'src/copy2public', to: '' }]),
     new WorkboxPlugin.InjectManifest({
       swSrc: './src/src-sw.js',
       swDest: 'sw.js',
